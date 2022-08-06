a8324bd9cb8afe201d21e646889afc02c247fe31
LotusBro98 <LotusBro98@yandex.ru>
2022-08-06 12:24:02 +0300

    Update process.py

--- process.py
+++ process.py
@@ -17,9 +17,10 @@ def log_spectrum(x, fs, fstep=FREQ_STEP, fmin=MIN_FREQ, fsave=SAVE_FREQ):
         subset = spec_all[int(fn[i]/np.sqrt(fstep)): int(fn[i]*np.sqrt(fstep))]
         spec = np.zeros((n_save,), dtype=np.complex128)
         center = len(subset)//2
-        n_save_i = min(len(subset) - center, center, n_save)
-        spec[:n_save_i] = subset[center:center+n_save_i]
-        spec[-n_save_i:] = subset[center-n_save_i:center]
+        end = min(len(subset), center + (n_save//2))
+        start = max(0, center - (n_save//2))
+        spec[:end-center] = subset[center:end]
+        spec[-(center-start):] = subset[start:center]
         ampl = np.fft.ifft(spec)
         log_spec.append(ampl)
     log_spec = np.stack(log_spec, axis=-1)
@@ -41,7 +42,6 @@ def generate(spectrum, fs, fstep=FREQ_STEP, fmin=MIN_FREQ, fsave=SAVE_FREQ):
         start = max(0, center - (n_save//2))
         subset[center:end] = spec[:end-center]
         subset[start:center] = spec[-(center-start):]
-        spec_all[int(fn[i]/np.sqrt(fstep)) : int(fn[i]*np.sqrt(fstep))] = subset
     spec_all[1:] += np.conj(spec_all)[1:][::-1]
     track = np.fft.ifft(spec_all)
     track = np.real(track)

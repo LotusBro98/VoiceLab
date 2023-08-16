57cb3d7511f858d550c25cb2d8647208d399715b
adpanyukov <adpanyukov@sberbank.ru>
2023-08-16 12:17:17 +0300

    improved generate. Now it fits almost perfectly. Adjusted parameters. Cleaned up main

--- main.py
+++ main.py
@@ -1,39 +1,25 @@
-from pyffmpeg import FFmpeg
-from scipy.io.wavfile import read, write
-import matplotlib.pyplot as plt
+import librosa
+from scipy.io.wavfile import write
 import numpy as np
 
 import notes
 from display import render_plot
-from process import generate, log_spectrum, harmonics, SAVE_FREQ
+from process import generate, log_spectrum
 
-# SOURCE = "data/Detektivbyrn_-_Om_Du_Mter_Varg_63265005.mp3"
+SOURCE = "data/Detektivbyrn_-_Om_Du_Mter_Varg_63265005.mp3"
 # SOURCE = "data/kukla_kolduna.mp3"
-SOURCE = "data/Alan_Jackson_-_Chattahoochee_48072565.mp3"
-# SOURCE = "Lenka.mp3"
+# SOURCE = "data/Alan_Jackson_-_Chattahoochee_48072565.mp3"
 # SOURCE = "data/Lenka.mp3"
-TEMP = "temp.wav"
 TIME_START = 0
 TIME_WINDOW = 10
 
-ff = FFmpeg()
-ff.convert(SOURCE, TEMP)
-
-sample_rate, track = read(TEMP)
-track = track[:,0]
-track = track / 2**15
+track, sample_rate = librosa.load(SOURCE)
 track = track[int(TIME_START*sample_rate): int((TIME_START+TIME_WINDOW)*sample_rate)]
 track0 = track
 
 spectrum = log_spectrum(track, sample_rate)
 
-harms = harmonics(spectrum)
-harms = harms[int(1.125 * SAVE_FREQ)]
-plt.plot(np.sum(np.abs((harms)), axis=-1))
-plt.show()
-
 render_plot(np.abs(spectrum), 0, sample_rate)
-render_plot(np.abs(harms.T), 0, sample_rate)
 
 track2 = generate(spectrum, sample_rate)
-write("track.wav", sample_rate, (track2*2**15).astype(np.int16))
+write("track.wav", sample_rate, (track2*2**31).astype(np.int32))
--- process.py
+++ process.py
@@ -1,15 +1,17 @@
-import matplotlib.pyplot as plt
 import numpy as np
+
 import notes
 
-FREQ_STEP = np.power(2, 1/12 / 1)
+FREQ_STEP = np.power(2, 1 / 12 / 3)
+WINDOW_FREQ_STEP = np.power(2, 1 / 12)
 MIN_FREQ = notes.NOTES["C1"]
-SAVE_FREQ = 100
+SAVE_FREQ = 256
 
 
 def get_window(n_save, win_size):
     window = (np.arange(n_save) + n_save // 2) % n_save - n_save // 2
-    window = np.exp(-np.square(window / win_size))
+    std = win_size / 3
+    window = np.exp(-0.5 * np.square(window / std))
     return window
 
 
@@ -32,50 +34,53 @@ def get_subset(spec_all, fn, fstep, n_save):
     return spec
 
 
-def log_spectrum(x, fs, fstep=FREQ_STEP, fmin=MIN_FREQ, fsave=SAVE_FREQ):
+def log_spectrum(x, fs, fstep=FREQ_STEP, win_fstep=WINDOW_FREQ_STEP, fmin=MIN_FREQ, fsave=SAVE_FREQ):
     n_save = int(len(x) * fsave / fs)
     spec_all = np.fft.fft(x)
-    nmax = int(np.log(fs / 2 / fmin) / np.log(fstep))
+    nmax = int(np.ceil(np.log(fs / 2 / fmin) / np.log(fstep)))
     fn = len(x) / fs * fmin * np.power(fstep, np.arange(0, nmax + 1))
     log_spec = []
     for i in range(len(fn)):
-        spec = get_subset(spec_all, fn[i], fstep, n_save)
+        spec = get_subset(spec_all, fn[i], win_fstep, n_save)
         ampl = np.fft.ifft(spec)
         log_spec.append(ampl)
     log_spec = np.stack(log_spec, axis=-1)
     return log_spec
 
 
-def generate(spectrum, fs, fstep=FREQ_STEP, fmin=MIN_FREQ, fsave=SAVE_FREQ):
+def set_subset(spec_all, fn, fstep, n_save, spec, weights):
+    win_size = fn * (fstep - 1)
+    win = get_window(n_save, win_size)
+
+    fni = int(fn)
+    idx_from = np.arange(fni, fni + n_save)
+    idx_from -= n_save // 2
+    idx_from %= spec_all.shape[-1]
+
+    idx_to = np.arange(0, n_save)
+    idx_to -= n_save // 2
+    idx_to %= n_save
+
+    spec_all[idx_from] += spec[idx_to]
+    weights[idx_from] += win[idx_to]
+
+    return spec
+
+
+def generate(spectrum, fs, fstep=FREQ_STEP, win_fstep=WINDOW_FREQ_STEP, fmin=MIN_FREQ, fsave=SAVE_FREQ):
     n_all = int(len(spectrum) * fs / fsave)
     n_save = spectrum.shape[0]
     spec_all = np.zeros((n_all,), dtype=np.complex128)
-    nmax = int(np.log(fs / 2 / fmin) / np.log(fstep))
+    weights_all = np.zeros((n_all,), dtype=np.complex128)
+    nmax = int(np.ceil(np.log(fs / 2 / fmin) / np.log(fstep)))
     fn = n_all / fs * fmin * np.power(fstep, np.arange(0, nmax + 1))
     for i in range(len(fn) - 1):
         ampl = spectrum[:,i]
         spec = np.fft.fft(ampl)
-        subset = spec_all[int(fn[i]/np.sqrt(fstep)): int(fn[i]*np.sqrt(fstep))]
-        center = len(subset)//2
-        end = min(len(subset), center + (n_save//2))
-        start = max(0, center - (n_save//2))
-        subset[center:end] = spec[:end-center]
-        subset[start:center] = spec[-(center-start):]
+        set_subset(spec_all, fn[i], win_fstep, n_save, spec, weights_all)
+    spec_all[len(spec_all) // 2:] = 0
+    spec_all /= weights_all.clip(1e-3, None)
     spec_all[1:] += np.conj(spec_all)[1:][::-1]
     track = np.fft.ifft(spec_all)
     track = np.real(track)
     return track
-
-
-def harmonics(spectrum, fstep=FREQ_STEP, n_harms=9):
-    mask_idx = np.int32(np.round(np.log(range(1, n_harms + 1)) / np.log(fstep)))
-    harms = np.stack([np.take(spectrum, mask_idx + i, axis=-1, mode='clip') for i in range(spectrum.shape[-1])], axis=-2)
-
-    for t in range(harms.shape[0]):
-        max_stacks = []
-        for i in range(1):
-            power = np.sum(np.abs(harms[t]), axis=-1)
-            power_max_i = np.argmax(power)
-            max_stacks.append((power_max_i, harms[t, power_max_i].copy()))
-
-    return harms

c7869fff5e771bd7c8182a1033e7cfa7180b250c
Alexander Panyukov <adpanyukov@sberbank.ru>
2025-01-13 14:34:37 +0300

    copies get_basis function

--- decompose.py
+++ decompose.py
@@ -6,6 +6,7 @@ import numpy as np
 import cv2 as cv
 
 from display import complex_picture
+from process import sample_to_freq, freq_to_sample, get_window, FREQ_RES
 
 
 def to_freq_diff_repr(spectrum):
@@ -15,6 +16,33 @@ def to_freq_diff_repr(spectrum):
     )
 
     return spectrum
+
+
+def get_voice_basis(n_freqs):
+    min_voice = 50
+    max_voice = 1000
+    base_freqs = sample_to_freq(
+        torch.linspace(
+            freq_to_sample(min_voice), 
+            freq_to_sample(max_voice),
+            200
+        )
+    )
+
+    basis = [torch.zeros(n_freqs)]
+    for freq in base_freqs:
+        harmonics = torch.zeros(n_freqs)
+        for i in range(1, 20):
+            fni = int(freq_to_sample(freq * i))
+            if fni >= n_freqs:
+                break
+            win = get_window(n_freqs, FREQ_RES)
+            harmonics += win.roll(fni)
+        basis.append(harmonics)
+    basis = torch.stack(basis, dim=0)
+
+    return basis
+
 
 def extract_voice(spectrum):
 

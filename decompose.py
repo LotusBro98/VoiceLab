23f10c89378e46eda1a4c52cbf93fa9967836a0b
Alexander Panyukov <adpanyukov@sberbank.ru>
2025-01-13 17:38:45 +0300

    complex voice basis

--- decompose.py
+++ decompose.py
@@ -4,12 +4,13 @@ import torch
 from torch import nn
 import numpy as np
 import cv2 as cv
+from scipy.special import erfinv
 
 from display import complex_picture
 from process import sample_to_freq, freq_to_sample, get_window, FREQ_RES
 
 
-def to_freq_diff_repr(spectrum):
+def to_freq_diff_repr(spectrum) -> torch.Tensor:
     spectrum = (
         spectrum.abs() *
         torch.exp(1j * (spectrum.angle() - spectrum.roll(-1, -2).angle()))
@@ -18,26 +19,43 @@ def to_freq_diff_repr(spectrum):
     return spectrum
 
 
-def get_voice_basis(n_freqs):
+def get_sample_scale_window(n_freqs, fni):
+    win_size = FREQ_RES
+    window = torch.arange(n_freqs) - fni
+    prob_outside = 1e-2
+    std = (win_size / 2) / erfinv(1 - prob_outside)
+    window = torch.exp(-0.5 * torch.square(window / std))
+
+    return window
+
+
+def get_voice_basis(n_freqs, n_basis=400):
     min_voice = 50
     max_voice = 1000
+    max_voice_full = 5000
     base_freqs = sample_to_freq(
         torch.linspace(
             freq_to_sample(min_voice), 
             freq_to_sample(max_voice),
-            200
+            n_basis
         )
     )
+    
+    sample_freqs = sample_to_freq(torch.arange(n_freqs))
 
-    basis = [torch.zeros(n_freqs)]
+    basis = []
     for freq in base_freqs:
-        harmonics = torch.zeros(n_freqs)
-        for i in range(1, 20):
-            fni = int(freq_to_sample(freq * i))
-            if fni >= n_freqs:
+        harmonics = torch.zeros(n_freqs, dtype=torch.complex64)
+        for i in range(1, 30):
+            freq_i = freq * i
+            if freq_i >= max_voice_full:
                 break
-            win = get_window(n_freqs, FREQ_RES)
-            harmonics += win.roll(fni)
+
+            fni = freq_to_sample(freq_i)
+            win = get_sample_scale_window(n_freqs, fni)
+            ampl = win
+            df = (sample_freqs - freq_i) / freq
+            harmonics += ampl * torch.exp(2j * torch.pi * df)
         basis.append(harmonics)
     basis = torch.stack(basis, dim=0)
 
@@ -46,14 +64,14 @@ def get_voice_basis(n_freqs):
 
 def extract_voice(spectrum):
 
+    basis = get_voice_basis(spectrum.shape[-1])
+
     spectrum *= torch.linspace(0, 1, spectrum.shape[-1])
     spectrum /= spectrum.abs().max()
 
     spectrum = to_freq_diff_repr(spectrum)
 
-    image = complex_picture(spectrum)
-
-    plt.imshow(image)
+    f, ax = plt.subplots(2, 1, figsize=(10, 10))
+    ax[0].imshow(complex_picture(spectrum))
+    ax[1].imshow(complex_picture(basis))
     plt.show()
-
-    # f, ax = plt.subplots

fixed incorrect window scaling

main.py
@@ -36,8 +36,9 @@ track0 = track
 builder = SpectrogramBuilder(sample_rate)
 
 spectrum = builder.encode(torch.tensor(track))
-
 print(spectrum.shape)
+track2 = builder.decode(spectrum)
+spec2 = builder.encode(track2)
 
 # spectrum = build_spectrogram(track, sample_rate)
 
@@ -45,13 +46,15 @@ print(spectrum.shape)
 
 # spectrum += 0.1 * torch.randn_like(spectrum)
 
-plt.figure(figsize=(15, 10))
-plt.imshow(builder.complex_picture(spectrum)[::-1], aspect=5, interpolation="nearest")
+f, ax = plt.subplots(3, figsize=(15, 20))
+ax[0].imshow(builder.complex_picture(spectrum)[::-1], aspect=5, interpolation="nearest")
+ax[1].imshow(builder.complex_picture(spec2)[::-1], aspect=5, interpolation="nearest")
+ax[2].imshow(builder.complex_picture(spec2 - spectrum)[::-1], aspect=5, interpolation="nearest")
 plt.savefig("complex_pic.png")
 plt.close()
 
 # render_plot(np.abs(spectrum), 0, sample_rate)
 
-track2 = builder.decode(spectrum).numpy()
-write("track.wav", sample_rate, (track2*2**31).astype(np.int32))
+
+write("track.wav", sample_rate, (track2.numpy()*2**31).astype(np.int32))
 write("orig.wav", sample_rate, (track*2**31).astype(np.int32))

process.py
@@ -95,10 +95,7 @@ class SpectrogramBuilder(nn.Module):
 
         t = torch.arange(0, win_size_T, 1 / self.sample_rate) - win_size_T / 2
 
-        W = (
-            1 / math.sqrt(len(t)) * 
-            torch.exp(2j * torch.pi * fn[:, None] * t[None, :])
-        )
+        W = torch.exp(2j * torch.pi * fn[:, None] * t[None, :])
 
         ksize = W.shape[-1]
         stride = int(self.sample_rate / self.fsave)
@@ -106,9 +103,12 @@ class SpectrogramBuilder(nn.Module):
         windows = self.get_window(W.shape[-1], (self.sample_rate / df)[:, None], W.shape[-1] // 2)
         mask = self.get_window_mask(ksize, stride, windows)
 
-        W_enc = W * windows
+        W *= windows
+        W /= W.abs().square().sum(-1, keepdim=True).sqrt()
 
-        W_dec = W * windows / mask
+        W_enc = W
+
+        W_dec = W / mask
 
         # plt.imshow(self.complex_picture(W))
         # plt.savefig("kernel.png")